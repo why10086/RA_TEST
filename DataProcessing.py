2fae15d6d447892c94a4674d6a79be4c1c003d5c
why10086 <1505064156@qq.com>
2022-07-28 16:05:39 +0800

    reload json file

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,12 +1,31 @@
 import json
 import csv
 
+data = []
 #读取csv文件
-filename = './question_1/q1_uncomplete.csv'
+filename = 'q1_uncomplete.csv'
 with open(filename, "r") as f:
 	reader = csv.DictReader(f)
 	for row in reader:
-		print(row)
+		data.append(row)
+f.close()
+for row in data:
+  jsonname = row['txn_hash'] + '.json'
+  f = open(jsonname, encoding='utf-8')
+  reader = json.load(f)
+
+  logs = reader['logs']
+  for log in logs:
+    if log['data'] == '0x':
+      address = log['address']
+      token_id = int(log['topics'][-1], 16)
+    else:
+      price_value = int(log['data'][-20:], 16)
+    print(address, token_id, price_value)
+
+
+
+
 
 
 
